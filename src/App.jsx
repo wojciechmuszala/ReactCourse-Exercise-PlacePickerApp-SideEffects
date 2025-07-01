066009bc7ef445682cf62b42957f15f7516e8a67
Wojciech Muszala <wojciech.muszala@gmail.com>
2025-07-01 19:47:06 +0200

    fix(modal): handle ESC button click to close the modal

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ function App() {
 
   return (
     <>
-      <Modal open={isModalOpen}>
+      <Modal open={isModalOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = ({ open, children }) => {
+const Modal = ({ children, open, onClose }) => {
   const dialog = useRef();
 
   useEffect(() => {
@@ -13,7 +13,7 @@ const Modal = ({ open, children }) => {
   }, [open]);
 
   return createPortal(
-    <dialog className='modal' ref={dialog}>
+    <dialog className='modal' ref={dialog} onClose={onClose}>
       {children}
     </dialog>,
     document.getElementById("modal")
